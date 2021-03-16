29911d5e95071fe6089a928a87e1e967e56221c4
Sumeet Charan <z5160224@ad.unsw.edu.au>
2021-03-16 23:16:22 +1100

    WebScraper foundations

--- PHASE_1/API_SourceCode.py
+++ PHASE_1/API_SourceCode.py
@@ -1,5 +1,7 @@
 import flask
 from flask import request, jsonify
+from datetime import datetime
+from WebScraper import WebScraper
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -25,6 +27,7 @@ def api_all():
 # testing taking params in the body
 @app.route('/some', methods=['GET'])
 def api_some():
+    scraper = WebScraper("webScraper", datetime.now())
+    scraper.returnScrapeData()
     return jsonify(request.data.decode("utf-8"))
-
 app.run()
