d5ad043f9b71825ebf8e0eecae22be78706ae853
harrygunasekera <harrygunasekera@yahoo.com.au>
2021-03-17 01:40:35 +1100

    added the filter concurrency and addition

--- PHASE_1/crawlerWHO.py
+++ PHASE_1/crawlerWHO.py
@@ -27,7 +27,7 @@ class crawlerWHO:
 		return self._homeURL
 	
 
-	def searchPage(self, keywords, startDate, endDate):
+	def searchPage(self, location, keywords, startDate, endDate):
 		content = self._page.soup.find(id="content")
 		content = content.select('a')
 		yearLinks = []
@@ -59,81 +59,44 @@ class crawlerWHO:
 					articleDateObj = datetime.strptime(articleDate, "%d %B %Y")
 					# check if in right time period
 					if articleDateObj >= startDateObj and articleDateObj <= endDateObj:
-						# if there are no keywords then return all articles within the time period
+						# if there are no keywords or a location then return all articles within the time period
 						if not keywords:
 							address = link["href"]
 							articleLinks.append(c.homeURL + address)
-							print(c.homeURL + address)
-						# if there are keywords then filter
-						else:
+						# if there are keyword AND a location
+						elif location and keywords:
 							# loop the element siblings to find the span with the description
 							for sibling in link.next_siblings:
 								if sibling.name == "span":
 									desc = sibling.text
-									# loop through each keyword and if there is a match in the description
-									# then include link
-									for word in keywords:
-										if word in desc:
+									# check if location is in the description if it is 
+									# then check the keywords
+									if location[0].lower() in desc.lower():
+										for word in keywords:
+											if word.lower() in desc.lower():
+												address = link["href"]
+												articleLinks.append(c.homeURL + address)
+						# if there are keywords OR a location but not both
+						else:
+							filterParams = keywords + location
+							# loop the element siblings to find the span with the description
+							for sibling in link.next_siblings:
+								if sibling.name == "span":
+									desc = sibling.text
+									# loop through each keyword or location and if there is a match 
+									# in the description then include link
+									for word in filterParams:
+										if word.lower() in desc.lower():
 											address = link["href"]
 											articleLinks.append(c.homeURL + address)
-											print(c.homeURL + address)
+		return articleLinks
 
 
 c = crawlerWHO()
 startDate = "2008-01-01T00:00:00"
 endDate = "2009-01-01T00:00:00"
-keywords = ["Avian"]
-c.searchPage(keywords, startDate, endDate)
-
-# p = c.page.soup
-# content = p.find(id='content')
-# content = content.select('a')
-
-# startDate = "2021-01-01T00:00:00"
-# endDate = "2021-03-16T00:00:00"
-# keywords = ["coronavirus"]
-# startYear = int(startDate[0:4])
-# endYear = int(endDate[0:4])
-# startDateObj = datetime.fromisoformat(startDate)
-# endDateObj = datetime.fromisoformat(endDate)
-# yearLinks = []
-# for link in content:
-# 	address = link["href"]
-# 	if re.search("/csr/don/archive/year/[0-9]{4}/en/", address):
-# 		# Isolate the links that would have data from the right period
-# 		year = re.findall("[0-9]{4}", address)
-# 		year = int(year[0])
-# 		if year >= startYear and year <= endYear:
-# 			yearLinks.append(c.homeURL + address)
-# # Open each year link and return the articles that are relevant
-# articleLinks = []
-# for yearLink in yearLinks:
-# 	yearPage = c.browser.get(yearLink)
-# 	yearContent = yearPage.soup.find(id="content")
-# 	yearContent = yearContent.select('a')
-# 	# Loop through each link
-# 	for link in yearContent:
-# 		# isolate the articles
-# 		if re.findall("[0-9]{1,2} [a-zA-Z]{3,9} [0-9]{4}", link.text):
-# 			# isolate date then convert to datetime object
-# 			articleDate = link.text
-# 			articleDateObj = datetime.strptime(articleDate, "%d %B %Y")
-# 			# check if in right time period
-# 			if articleDateObj >= startDateObj and articleDateObj <= endDateObj:
-# 				# if there are no keywords then return all articles within the time period
-# 				if not keywords:
-# 					address = link["href"]
-# 					print(c.homeURL + address)
-# 				# if there are keywords then filter
-# 				else:
-# 					# loop the element siblings to find the span with the description
-# 					for sibling in link.next_siblings:
-# 						if sibling.name == "span":
-# 							desc = sibling.text
-# 							# loop through each keyword and if there is a match in the description
-# 							# then include link
-# 							for word in keywords:
-# 								if word in desc:
-# 									address = link["href"]
-# 									articleLinks.append(c.homeURL + address)
-# 									print(c.homeURL + address)
+keywords = ["cholera"]
+location = ["Iraq"]
+links = c.searchPage(location, keywords, startDate, endDate)
+for link in links:
+	print(link)
