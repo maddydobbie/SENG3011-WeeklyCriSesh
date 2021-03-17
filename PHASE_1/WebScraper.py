7e8a5d25497377b95d2414b9f0013013fe45fda9
harrygunasekera <harrygunasekera@yahoo.com.au>
2021-03-17 23:13:04 +1100

    webscraper updated

--- PHASE_1/WebScraper.py
+++ PHASE_1/WebScraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pickle
+import re
 
 class WebScraper():
     def __init__(self, name, creationTime):
@@ -11,16 +12,80 @@ class WebScraper():
         with open('objects/diseases' + '.pkl', 'rb') as f:
             self._diseases = pickle.load(f)
 
+    def checkRegexString(self, regexString, text):
+        if (re.search(regexString, text)):
+            return True
+        else:
+            return False
+
+    def getDateFromRegex(self, regexString, text):
+        dateText = re.search(regexString, text)[0] #extracting the date from the text
+        #Lists for different months
+        monthFullList = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+        monthShortList = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "SEP", "OCT", "NOV", "DEC"]
+        #if date format is in the form DD Month (in words) YYYY
+        dateString = ""
+        if (self.checkRegexString(r"[0-9]{2}-[a-z]+-[0-9]+", text)):
+            day = (re.search("[0-9]{2}", dateText))[0]
+            month = (re.search("[a-zA-Z]{3,}", dateText))[0].upper()
+            year = (re.search("[0-9]{4}", dateText))[0]
+            #extracting month of different formats (e.g. Feb vs February)
+            if (month in monthFullList):
+                index = monthFullList.index(month)
+            elif (month in monthShortList):
+                index = monthShortList.index(month)
+            month = monthFullList[index]
+            #combining all information
+
+        elif (self.checkRegexString(r"[0-9]{4}_[0-9]{2}_[0-9]{2}", text)):
+            #attaining all info in YYYY_MM_DD format
+            year = (re.search("[0-9]{4}", dateText))[0]
+            monthIndex = (re.search("_[0-9]{2}_", dateText))[0]
+            monthIndex = int(monthIndex[1:-1]) - 1
+            month = monthFullList[monthIndex]
+            day = (re.search("[0-9]{2}$", dateText))[0]
+
+        dateString += day + ' ' + month + ' ' + year
+        return dateString
+
+    def checkDate(self, URL):
+        if (self.checkRegexString(r"[0-9]{2}-[a-z]+-[0-9]+", URL)):
+            Date = self.getDateFromRegex(r"[0-9]{2}-[a-z]+-[0-9]+", URL)
+        elif (self.checkRegexString(r"[0-9]{4}_[0-9]{2}_[0-9]{2}",URL)):
+            Date = self.getDateFromRegex(r"[0-9]{4}_[0-9]{2}_[0-9]{2}", URL)
+        else:
+            Date = "Could not be found"
+        return Date
+
+    def checkCountry(self, title):
+        if (self.checkRegexString(r"in\s[a-zA-z\s]*", title)):
+            country = (re.search(r"in\s[a-zA-z\s]*", title))[0]
+            country = country[3:]
+        elif (self.checkRegexString(r"– .+", title)):
+            country = (re.search(r"– .+", title))[0]
+            country = country[2:]
+        else:
+            print("Country cannot be found")
+            country = "Cannot be found."
+        return country
+
+
     def returnScrapeData(self, links):
         articles = []
         for URL in links:
             page = requests.get(URL)
             pageParse = BeautifulSoup(page.content, 'html.parser')
-            print("URL: ", page.url) #url used for date
-            title = pageParse.find("meta",property ='og:title')
-            print("TITLE: ", title) #title used for country
 
-            print ("Data crawled and returned")
+            pageURl = page.url #url used for date
+            title = (pageParse.find_all(class_="headline")[0]).text
+            dateString = self.checkDate(pageURl)
+            country = self.checkCountry(title)
+
+            print("URL:", pageURl)
+            print("Title:", title)
+            print("Date:", dateString)
+            print("Country:", country)
+
             symptoms = []
             diseases = []
             # isolate the body of the html
