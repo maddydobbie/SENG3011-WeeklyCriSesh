a23e1231275c1d2651e84fda6f106fec4338885b
maddydobbie <z5112961@student.unsw.edu.au>
2021-03-26 16:29:48 +1100

    fixed date month issue]

--- PHASE_1/API_SourceCode/API_SourceCode.py
+++ PHASE_1/API_SourceCode/API_SourceCode.py
@@ -66,9 +66,24 @@ def api_articles():
 			response.error_type = "Bad Request"
 			response.status_code = 400
 			response._content = b'{ "reason" : "End date in incorrect format." }'
+		try:
+			end = datetime.fromisoformat(jsonData['endDate'])
+		except ValueError:
+			response.error_type = "Bad Request"
+			response.status_code = 400
+			response._content = b'{ "reason" : "End date in incorrect format." }'
+			return (response.text, response.status_code, response.headers.items())
+		try:
+			start = datetime.fromisoformat(jsonData['startDate'])
+		except ValueError:
+			response.error_type = "Bad Request"
+			response.status_code = 400
+			response._content = b'{ "reason" : "Start date in incorrect format." }'
+			return (response.text, response.status_code, response.headers.items())
+			
 		# check correct order 
 		# check either date isn't in the future 
-		elif datetime.fromisoformat(jsonData['startDate']) > datetime.fromisoformat(jsonData['endDate']):
+		if datetime.fromisoformat(jsonData['startDate']) > datetime.fromisoformat(jsonData['endDate']):
 			response.error_type = "Bad Request"
 			response.status_code = 400
 			response._content = b'{ "reason" : "End date before start date." }'
